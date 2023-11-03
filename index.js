acccc646a889e6ea180325c37177a20f5761148a
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2023-11-03 23:03:37 +0530

    bug fix: change the formula

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // random number generator in 6 digits
 const randomNumberGenerator = (min = 100000, max = 999999) => {
-  return Math.round(Math.random() * (max - min) + max);
+  return Math.round(Math.random() * (max - min) + min);
 };
 
 export default randomNumberGenerator;
